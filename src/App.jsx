8ed7f14513060d35fe2b7cdc86dfeb9c835f194a
o6b7 <alarkie2210@gmail.com>
2025-09-02 08:35:54 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {ScrollTrigger, SplitText} from "gsap/all";
+import gsap from "gsap";
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+    return (
+        <main className="App">
+        </main>
+    )
+}
+
+export default App;
